b9342e1df69105e4b239b58fea109cbc0ed0cd85
Tautvydas Tunkis <ttunkis@gmail.com>
2022-04-13 13:52:12 +0300

    header html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-  return <div className='App'>
-    <h1>React</h1>
-  </div>;
+  return (
+    <div className='App'>
+      <Header />
+      <Route path={'/'} exact>
+        <h1>Home page</h1>
+      </Route>
+      <Route path={'/login'}>
+        <LoginPage />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className='header'>
+      <h2>System</h2>
+      <nav>
+        <NavLink to={'/'}>Home</NavLink>
+        <NavLink to={'/vip'}>Vip</NavLink>
+        <NavLink to={'/login'}>Login</NavLink>
+        <NavLink to={'/logout'}>Logout</NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
